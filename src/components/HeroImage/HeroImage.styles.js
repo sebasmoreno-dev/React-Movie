2392b45577aeaee9df91c5b95f2ce031a0c195d9
Sebastian Moreno <sebastian.moreno.contacto@gmail.com>
2022-02-02 22:14:14 -0500

    New feature: add searchBar and thumb	✨ :sparkles:

--- src/components/HeroImage/HeroImage.styles.js
+++ src/components/HeroImage/HeroImage.styles.js
@@ -1,7 +1,65 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div``;
+//1. se coloca como priroidad el linear gradient.
+//2. el linear gradient va dentro de background-image
+//3. Despues la variable yfinalmente la url con la imagen
+export const Wrapper = styled.div`
+  background-image:
+    linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0.2) 41%,
+      rgba(0, 0, 0, 0.65) 100%,
+    var(--darkGrey)),
+    url(${({ image }) => image});
+  background-size: 100%, cover;
+  background-position: center;
+  height: 600px;
+  position: relative;
+  animation: animateHeroImage 1s;
 
-export const Content = styled.div``;
+  @keyframes animateHeroImage {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+`;
 
-export const Text = styled.div``;
+export const Content = styled.div`
+  padding: 20px;
+  max-width: var(--maxWidth);
+  margin: 0 auto;
+`;
+
+export const Text = styled.div`
+  z-index: 100;
+  max-width: 700px;
+  position: absolute;
+  bottom: 40px;
+  margin-right: 20px;
+  margin-left: 100px;
+  color: var(--white);
+
+  h1 {
+    font-size: var(--fontSuperBig);
+
+    @media screen and (min-width: 720px) {
+      font-size: var(--fontBig);
+    }
+  }
+
+  p {
+    font-size: var(--fontMed);
+
+    @media screen  and (max-width: 720px) {
+      font-size: var(--fontSmall);
+    }
+  }
+
+  @media screen and (max-width: 720px) {
+    max-width: 100%;
+    margin-left: 30px;
+  }
+`;
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,11 @@ import React, { useState, useEffect } from 'react';
 //config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from './../config';
 //Components
-import HeroImage from './HeroImage';
+import HeroImage from './HeroImage/HeroImage';
+import Grid from './Grid';
+import Thumb from '../Thumb';
+import Spinner from './Spinner';
+import SearchBar from './SearchBar';
 //Hook
 import { useHomeFetch } from './../hooks/useHomeFetch';
 
@@ -11,22 +15,37 @@ import { useHomeFetch } from './../hooks/useHomeFetch';
 import NoImage from './../images/no_image.jpg';
 
 const Home = () => {
-  const { state, loading, error } = useHomeFetch();
+  const { state, loading, error, setSearchTerm } = useHomeFetch();
 
   console.log(state);
 
   return (
     <>
-      {state.results[0] ?
+      {state.results[0] ? (
         <HeroImage
           image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
           title={state.results[0].original_title}
           text={state.results[0].overview}
         />
-      : null
-      }
+      ) : null}
+      <SearchBar setSearchTerm={setSearchTerm} />
+      <Grid header='Popular Movies'>
+        {state.results.map(movie => (
+          <Thumb
+            key={movie.id}
+            clickable
+            image={
+              movie.poster_path
+                ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                : NoImage
+            }
+            movieId={movie.id}
+          />
+        ))}
+      </Grid>
+      <Spinner />
     </>
-  )
-}
+  );
+};
 
 export default Home;

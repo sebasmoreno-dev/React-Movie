12665652f10897edec11d17691783a786fe9178a
Sebastian Moreno <sebastian.moreno.contacto@gmail.com>
2022-02-03 11:46:17 -0500

    New feature: Functionallity of searchBar and LoadMore Movies	✨ :sparkles:

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,7 @@ import Grid from './Grid';
 import Thumb from '../Thumb';
 import Spinner from './Spinner';
 import SearchBar from './SearchBar';
+import Button from './Button';
 //Hook
 import { useHomeFetch } from './../hooks/useHomeFetch';
 
@@ -15,13 +16,21 @@ import { useHomeFetch } from './../hooks/useHomeFetch';
 import NoImage from './../images/no_image.jpg';
 
 const Home = () => {
-  const { state, loading, error, setSearchTerm } = useHomeFetch();
+  const { state,
+    loading,
+    error,
+    searchTerm,
+    setSearchTerm,
+    setIsLoadingMore
+  } = useHomeFetch();
 
   console.log(state);
 
+  if (error) return <div>Something went wrong 😮...</div>
+
   return (
     <>
-      {state.results[0] ? (
+      {!searchTerm && state.results[0] ? (
         <HeroImage
           image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
           title={state.results[0].original_title}
@@ -29,7 +38,7 @@ const Home = () => {
         />
       ) : null}
       <SearchBar setSearchTerm={setSearchTerm} />
-      <Grid header='Popular Movies'>
+      <Grid header={searchTerm ? 'Search Result' : 'Popular Movies'}>
         {state.results.map(movie => (
           <Thumb
             key={movie.id}
@@ -43,7 +52,10 @@ const Home = () => {
           />
         ))}
       </Grid>
-      <Spinner />
+      {loading && <Spinner />}
+      {state.page <state.total_pages && !loading && (
+        <Button text='Load More' callback={() => setIsLoadingMore(true)}/>
+      )}
     </>
   );
 };

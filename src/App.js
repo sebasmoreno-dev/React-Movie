5baccab97845dab3a39642508cfa2caf91c35a95
Sebastian Moreno <sebastian.moreno.contacto@gmail.com>
2022-02-03 19:34:24 -0500

    Continuous Integration 💚 :green_heart: Added BreadCrumb Component, MovieInfo Component, MovieInfoBar Component and his styles

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
 
+//Routes
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 //Components
 import Header from './components/Header';
 import Home from './components/Home';
+import Movie from './components/Movie';
+import NotFound from './components/NotFound';
 
 //Styles
 import { GlobalStyle } from './GlobalStyle';
 
-function App() {
-  return (
-    <div className="App">
+//Routes
+const App = () =>  (
+    <Router>
       <Header />
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/:movieId' element={<Movie />}/>
+        <Route path='/*' element={<NotFound />}/>
+      </Routes>
       <GlobalStyle />
-    </div>
+    </Router>
   );
-}
+
 
 export default App;
